7c5857eadda2b3e617463e2ecef1cc4b58827b1c
nuur-r <firdausnuurr@gmail.com>
2022-10-12 17:41:28 +0700

    new model

--- test.py
+++ test.py
@@ -1,13 +1,18 @@
-from cvzone.FaceMeshModule import FaceMeshDetector
+from cvzone.FaceDetectionModule import FaceDetector
 import cv2
 
 cap = cv2.VideoCapture(0)
-detector = FaceMeshDetector(maxFaces=2)
+detector = FaceDetector()
+
 while True:
     success, img = cap.read()
-    img, faces = detector.findFaceMesh(img)
-    if faces:
-        print(faces[0])
+    img, bboxs = detector.findFaces(img)
+
+    if bboxs:
+        # bboxInfo - "id","bbox","score","center"
+        center = bboxs[0]["center"]
+        cv2.circle(img, center, 5, (255, 0, 255), cv2.FILLED)
+
     cv2.imshow("Image", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
